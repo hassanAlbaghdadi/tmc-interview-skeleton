10d475d0ebfb330753df47a71ad0053fbfc338cc
hassan <hassanalbaghdadi@bluedrop.com>
2021-12-23 15:55:08 -0400

    first commit

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,18 +4,42 @@ document.addEventListener("DOMContentLoaded", async function() {
   const nav = document.getElementById("region-select")
   const detailsHeader = document.querySelector("#region-details h2")
   const detailsInfo = document.querySelector("#region-details p")
+  const provinceInfo = document.querySelector("#region-details div")
   
   provinces.forEach(province => {
     const button = document.createElement("button")
     button.innerText = province.short
     button.classList.add("region-option")
     
-    button.addEventListener("click", function() {
+    button.addEventListener("click", async function() {
+      provinceInfo.innerHTML = detailsInfo.innerHTML = ""
+      const cities = await fetch(`/api/cities/${province.name}`).then(res => res.json());
       [...nav.children].forEach(child => child.classList.remove("selected"))
       this.classList.add("selected")
-      detailsHeader.innerText = province.name
-      detailsInfo.innerHTML = `<h3>Capital</h3> ${province.capital}`
+      // @ts-ignore
+      detailsHeader.innerText = province.name;
+      if(cities.length > 0){
+        var table = createCitiesTable(cities, province.capital)
+        provinceInfo.append(table);
+      }
+      else{
+        detailsInfo.innerHTML = `No cities to display from the database.`
+      } 
     })
     nav.appendChild(button)
   })
-})
+});
+
+function createCitiesTable(cities, capital){
+  var table = document.createElement("table")
+  var row = table.insertRow(0)
+  row.classList.add('bold')
+  row.insertCell(0).innerHTML = "Cities"
+  row.insertCell(1).innerHTML = "Population";
+  [...cities].forEach((city, index) => {
+    var row = table.insertRow(index+1);
+    row.insertCell(0).innerHTML = city.Municipality === capital ? `<b>${city.Municipality}</b> (capital)` :  city.Municipality
+    row.insertCell(1).innerHTML = city["Population(2016)"]
+  });
+  return table;
+}
--- routes/api.js
+++ routes/api.js
@@ -12,4 +12,13 @@ router.get('/cities', function(req, res, next) {
   res.json(db.cities)
 });
 
+router.get('/cities/:province', function(req, res, next) {
+  res.json(db.cities.filter(x=>x.Province === req.params.province)
+                    .sort(function(a, b) {
+                      var r = parseFloat(a["Population(2016)"].replace(/,/g, '')) - parseFloat(b["Population(2016)"].replace(/,/g, ''));
+                      return r;
+                    })
+          )
+});
+
 module.exports = router;
